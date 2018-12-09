04e3fa301f43b06d66d7647f5abb11264777c873
charpty <charpty@gmail.com>
2018-12-09 19:56:07 +0800

    修改：将结果文章内容输出到系统剪切板

--- generate_blog_content_md.py
+++ generate_blog_content_md.py
@@ -2,14 +2,21 @@
 # -*- coding: UTF-8 -*-
 import sys
 import generate_articles_sql as ga
+import subprocess
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
+def write_to_clipboard(output):
+    process = subprocess.Popen(
+        'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+    process.communicate(output.encode('utf-8'))
+
+
 def main():
     args = sys.argv
-    file_name = "write-red-black-tree"
+    file_name = "redis-trs-modules-intro"
     files_dict = ga.get_files_dict()
     configs_dict = ga.get_configs_dict(files_dict)
     c_file = files_dict[file_name]
@@ -19,7 +26,10 @@ def main():
     with open(c_file, 'r') as f:
         content = f.read()
         content = ga.turn_image_link(content, "http://s.charpty.com/")
+        # print "\n"
         # print content
+        # 拷贝到osx剪切板
+        write_to_clipboard(content)
     with open("current_md_content.md", "wb") as f:
         f.write(content)
         f.close
